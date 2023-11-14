32579fd1478bd03661f32d3506ff0f4539c4b2df
x91061jp <john.price-3@student.manchester.ac.uk>
2023-11-14 16:32:44 +0000

    implemented working gameloop

--- main.py
+++ main.py
@@ -10,25 +10,31 @@ class AlienAnnihilator:
     def __init__(self, master):
         self.master = master  # initialises master window
         self.game_start = False  # the game loop doesn't initially run
-        self.configure_window()
+        self.start_button = ttk.Button(self.master, text="START", command=self.start_game)
         self.canvas = tk.Canvas(master, width=800, height=600, bg="black")
         self.menu()
 
-    def configure_window(self):
+    def configure_window(self, colour):
         self.master.geometry("800x600")
-        self.master.configure(background='#b3ffff')
+        self.master.configure(background=colour)
         self.master.title("Alien Annihilator")
 
-    def menu(self):
-        start_button = ttk.Button(window, text="START", command=self.start_game())
-        start_button.pack()
+    def menu(self, event=None):
+        self.game_start = False
+        self.start_button.pack()
+        self.configure_window("#b3ffff")
 
     def start_game(self):
-        self.canvas.delete("all")
         self.game_start = True
+        self.start_button.pack_forget()
+        self.configure_window("#000000")
+        self.master.bind("<KeyPress-Escape>", self.menu)
+        self.master.after(10, self.game_loop())
 
     def game_loop(self):
-        pass
+        if self.game_start:
+            print("wow")
+            self.master.after(10, self.game_loop)
 
 
 game = AlienAnnihilator(window)
