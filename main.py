56d20aaf6249a7a6a3bc330bee2e7aa5745d9dcd
x91061jp <john.price-3@student.manchester.ac.uk>
2023-11-14 09:14:38 +0000

    removed irrelevant methods

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ window = tk.Tk()
 class AlienAnnihilator:
     def __init__(self, master):
         self.master = master  # initialises master window
-        self.gameStart = False  # the game loop doesn't initially run
+        self.game_start = False  # the game loop doesn't initially run
         self.configure_window()
+        self.canvas = tk.Canvas(master, width=800, height=600, bg="black")
         self.menu()
 
     def configure_window(self):
         self.master.geometry("800x600")
-        self.master.configure(background="#b3ffff")
-        window.title("Alien Annihilator")
+        self.master.configure(background='#b3ffff')
+        self.master.title("Alien Annihilator")
 
     def menu(self):
         start_button = ttk.Button(window, text="START", command=self.start_game())
         start_button.pack()
 
     def start_game(self):
-        self.gameStart = True
+        self.canvas.delete("all")
+        self.game_start = True
 
     def game_loop(self):
-        self.update()
-
-    def update(self):
         pass
 
 
