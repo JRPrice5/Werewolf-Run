6d0445c9b84cc13e3880810583a9a95d2b00a946
x91061jp <john.price-3@student.manchester.ac.uk>
2023-11-16 12:54:23 +0000

    added menu layout

--- main.py
+++ main.py
@@ -10,24 +10,33 @@ class AlienAnnihilator:
     def __init__(self, master):
         self.master = master  # initialises master window
         self.game_start = False  # the game loop doesn't initially run
-        self.start_button = ttk.Button(self.master, text="START", command=self.start_game)
-        self.canvas = tk.Canvas(master, width=800, height=600, bg="black")
+        # self.start_button = ttk.Button(self.master, text="START", command=self.start_game)
+        self.canvas = tk.Canvas(master, width=800, height=600, bg="blue")
+        self.canvas.place(x=50, y=50)
+        self.start_button = tk.Button(self.master, text="START", width=15, height=2, command=self.start_game)
+        self.leaderboard_button = tk.Button(self.master, text="LEADERBOARD", width=15, height=2, command=self
+                                            .start_game)
+        self.bindings_button = tk.Button(self.master, text="KEY BINDINGS", width=15, height=2, command=self.start_game)
+        self.canvas.create_text(400, 150, text="ALIEN ANNIHILATOR", fill="black", font=("Arial Bold", 40))
         self.menu()
 
-    def configure_window(self, colour):
+    def configure_window(self):
         self.master.geometry("800x600")
-        self.master.configure(background=colour)
         self.master.title("Alien Annihilator")
 
     def menu(self, event=None):
         self.game_start = False
-        self.start_button.pack()
-        self.configure_window("#b3ffff")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.configure_window()
+        self.canvas.create_window(400, 250, window=self.start_button)
+        self.canvas.create_window(400, 300, window=self.leaderboard_button)
+        self.canvas.create_window(400, 350, window=self.bindings_button)
+        self.configure_window()
 
     def start_game(self):
         self.game_start = True
         self.start_button.pack_forget()
-        self.configure_window("#000000")
+        self.configure_window()
         self.master.bind("<KeyPress-Escape>", self.menu)
         self.master.after(10, self.game_loop())
 
@@ -38,4 +47,7 @@ class AlienAnnihilator:
 
 
 game = AlienAnnihilator(window)
-window.mainloop()
+window.mainloop()  # tells Python to run the Tkinter event loop.
+# This method listens for events, such as button clicks or keypresses,
+# and blocks any code that comes after it from running until you close
+# the window where you called the method.
